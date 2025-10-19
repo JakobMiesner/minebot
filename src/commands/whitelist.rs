b4b8da32f6d4c96459fa73b6711bf9efe9580afb
jakob miesner <jakob.miesner@gmail.com>
2025-10-19 14:19:16 +0200

    whitelist: ensure whitelist command only accepts MC names

--- src/commands/whitelist.rs
+++ src/commands/whitelist.rs
@@ -1,8 +1,15 @@
+use once_cell::sync::Lazy;
+use regex::Regex;
 use serenity::all::{CreateCommandOption, Permissions};
 use serenity::builder::CreateCommand;
 use serenity::model::application::{ResolvedOption, ResolvedValue};
 
 use crate::minecraft;
+
+fn is_valid_mc_username(s: &str) -> bool {
+  static RE: once_cell::sync::Lazy<Regex> = once_cell::sync::Lazy::new(|| Regex::new(r"^[A-Za-z0-9_]{3,16}$").unwrap());
+  RE.is_match(s)
+}
 
 pub async fn run<'a>(options: &'a [ResolvedOption<'a>]) -> String {
   let subcommand = options.first();
@@ -13,28 +20,34 @@ pub async fn run<'a>(options: &'a [ResolvedOption<'a>]) -> String {
     ..
   }) = subcommand
   {
-    let name = name.to_owned();
+    let command = name.to_owned();
 
-    if name == "list" {
-      let result = minecraft::rcon_command("whitelist list").await;
-      let mut players = result.find(':').map(|i| &result[i + 2..]).unwrap_or(result.as_str()).split(", ").collect::<Vec<&str>>();
-
-      players.sort_by(|a, b| a.to_lowercase().cmp(&b.to_lowercase()));
-
-      return players.iter().map(|player| format!("- {}", player.replace("_", "\\_"))).collect::<Vec<String>>().join("\n");
-    }
-
-    if let Some(ResolvedOption {
-      value: ResolvedValue::String(username),
-      ..
-    }) = subcommand_options.first()
-    {
-      match name {
-        "add" | "remove" => minecraft::rcon_command(&format!("whitelist {} {}", name, username)).await.replace("_", "\\_"),
-        _ => "Unknown subcommand".to_string(),
+    match command {
+      "add" | "remove" => {
+        if let Some(ResolvedOption {
+          value: ResolvedValue::String(username),
+          ..
+        }) = subcommand_options.first()
+        {
+          if (is_valid_mc_username(username)) {
+            minecraft::rcon_command(&format!("whitelist {} {}", command, username)).await.replace("_", "\\_")
+          } else {
+            "Not a valid username".to_string()
+          }
+        } else {
+          "No username provided".to_string()
+        }
       }
-    } else {
-      "No username provided".to_string()
+      "list" => {
+        minecraft::rcon_command("whitelist reload").await;
+        let result = minecraft::rcon_command("whitelist list").await;
+        let mut players = result.find(':').map(|i| &result[i + 2..]).unwrap_or(result.as_str()).split(", ").collect::<Vec<&str>>();
+
+        players.sort_by(|a, b| a.to_lowercase().cmp(&b.to_lowercase()));
+
+        return players.iter().map(|player| format!("- {}", player.replace("_", "\\_"))).collect::<Vec<String>>().join("\n");
+      }
+      _ => "Unknown subcommand".to_string(),
     }
   } else {
     "No subcommand provided".to_string()
